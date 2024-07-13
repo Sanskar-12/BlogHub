fc1bee7aa42d75f6d7756814c1ea879ed4a87268
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-13 15:34:49 +0530

    test api added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import {config} from "dotenv"
 import connectDB from "./db/connectDB.js"
+import userRoute from "./routes/user.route.js"
 
 config({
     path:"./.env"
@@ -9,6 +10,8 @@ config({
 const app=express()
 
 connectDB()
+
+app.use("/api/user",userRoute)
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening on port ${process.env.PORT}`)
