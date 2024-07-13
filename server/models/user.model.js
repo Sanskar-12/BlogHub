0d05cfa3852459c33896d502cf03a5671316a548
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-13 15:26:21 +0530

    user model added

---
+++ server/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User=mongoose.model("User",schema)
