a91116d35857d4faf30749fe96590c2924231090
Sanskar-12 <sanskarv2004@gmail.com>
2024-09-01 19:09:33 +0530

    Dash Comp Continue

--- frontend/src/components/DashComp.jsx
+++ frontend/src/components/DashComp.jsx
@@ -1,8 +1,83 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { server } from "../redux/store";
 
 const DashComp = () => {
-  return (
-    <div>DashComp</div>
-  )
-}
+  const [users, setUsers] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+  const [lastMonthUsers, setLastMonthUsers] = useState(0);
+  const [lastMonthPosts, setLastMonthPosts] = useState(0);
+  const [lastMonthComments, setLastMonthComments] = useState(0);
 
-export default DashComp
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/user/get/all/users?limit=5`,
+          {
+            withCredentials: true,
+          }
+        );
+
+        if (data.success) {
+          setUsers(data.users);
+          setTotalUsers(data.totalUsers);
+          setLastMonthUsers(data.lastMonthUsers);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    const fetchComments = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/comment/get/all/comments?limit=5`,
+          {
+            withCredentials: true,
+          }
+        );
+
+        if (data.success) {
+          setComments(data.comments);
+          setTotalComments(data.totalComments);
+          setLastMonthComments(data.lastMonthCreatedComments);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axios.get(`${server}/post/get/all/posts`, {
+          withCredentials: true,
+        });
+
+        if (data.success) {
+          setPosts(data.posts);
+          setTotalPosts(data.totalPosts);
+          setLastMonthPosts(data.lastMonthCreatedPosts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    if (currentUser?.user?.isAdmin) {
+      fetchUsers();
+      fetchComments();
+      fetchPosts();
+    }
+  }, [currentUser]);
+  return <div>DashComp</div>;
+};
+
+export default DashComp;
